ba20166dd25a1a4f5bf62841d2801f45d771608c
Alek Novkovski <alek@greatcode2.com>
2023-10-26 20:25:07 +0200

    Prettier reformat

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -4,31 +4,31 @@ import classes from './Navigation.module.css';
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
-  const ctx = React.useContext(AuthContext);
+    const ctx = React.useContext(AuthContext);
 
-  return (
-    <>
-                <nav className={classes.nav}>
-                    <ul>
-                        {ctx.isLoggedIn && (
-                            <li>
-                                <a href="/">Users</a>
-                            </li>
-                        )}
-                        {ctx.isLoggedIn && (
-                            <li>
-                                <a href="/">Admin</a>
-                            </li>
-                        )}
-                        {ctx.isLoggedIn && (
-                            <li>
-                                <button onClick={props.onLogout}>Logout</button>
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-    </>
-            )
+    return (
+        <>
+            <nav className={classes.nav}>
+                <ul>
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <a href="/">Users</a>
+                        </li>
+                    )}
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <a href="/">Admin</a>
+                        </li>
+                    )}
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <button onClick={props.onLogout}>Logout</button>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </>
+    )
 };
 
 export default Navigation;
